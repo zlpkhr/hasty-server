8fe5aa225a46fcead518471a7b8af12b031e72df
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-01-07 06:02:39 +0100

    Fix missing docs in tsc and eslint ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ["doc/"] },
+  { ignores: ["docs/"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
